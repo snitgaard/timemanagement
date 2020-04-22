95d2f5532a3abfb0631553d6f636a90888bd06b2
dreamy <31621059+dreamyy1@users.noreply.github.com>
2020-04-22 10:21:53 +0200

    what

--- src/timemanagement/gui/controller/MainUserViewController.java
+++ src/timemanagement/gui/controller/MainUserViewController.java
@@ -5,9 +5,19 @@
  */
 package timemanagement.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -17,6 +27,41 @@ import javafx.fxml.Initializable;
 public class MainUserViewController implements Initializable
 {
 
+    @FXML
+    private SplitPane timeLoggerPane;
+    @FXML
+    private JFXComboBox<?> projektComboBox;
+    @FXML
+    private JFXComboBox<?> opgaveComboBox;
+    @FXML
+    private JFXTextField startTidField;
+    @FXML
+    private JFXTextField slutTidField;
+    @FXML
+    private JFXTextField brugtTidField;
+    @FXML
+    private JFXTextField sagsNrField;
+    @FXML
+    private JFXCheckBox betaltCheckBox;
+    @FXML
+    private JFXTextField kundeField;
+    @FXML
+    private JFXTextField medarbejderField;
+    @FXML
+    private JFXTextField timeField;
+    @FXML
+    private JFXTextField titelField;
+    @FXML
+    private JFXTextArea beskrivelseTextArea;
+    @FXML
+    private JFXDatePicker datoField;
+    @FXML
+    private Pane opgaverPane;
+    @FXML
+    private JFXButton timeLoggerButton;
+    @FXML
+    private JFXButton opgaverButton;
+
     /**
      * Initializes the controller class.
      */
@@ -24,6 +69,18 @@ public class MainUserViewController implements Initializable
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
-    }    
+    }
+    
+    private void handleClicks(ActionEvent actionEvent)
+    {
+        if(actionEvent.getSource() == timeLoggerButton)
+        {
+            timeLoggerPane.toFront();
+        }
+        if(actionEvent.getSource() == opgaverButton)
+        {
+            opgaverPane.toFront();
+        }
+    }
     
 }

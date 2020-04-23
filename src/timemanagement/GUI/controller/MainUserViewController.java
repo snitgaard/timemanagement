0c412b8a9ac82b699d65136ce7408caafd6222d8
SerireS <55481182+SerireS@users.noreply.github.com>
2020-04-23 11:15:08 +0200

    Seneste nyt

--- src/timemanagement/GUI/controller/MainUserViewController.java
+++ src/timemanagement/GUI/controller/MainUserViewController.java
@@ -178,12 +178,10 @@ public class MainUserViewController implements Initializable
                
                long input = timeUsed;
                long hours = (input - input%3600)/3600;
-               long minutes = (input%3600 - input%3600%60)/60;
+               long minutes = (input%3600 - (input%3600%60))/60;
                long seconds = input%3600%60;
-               System.out.println("Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds);
-               
                brugtTidField.setText(hours +  " Hours  " + minutes + " Minutes  " + seconds + " Seconds  ");
-               
+
                
                 
                 

f167ffb83a7fe9acadcd58a327a556a14ffcee9f
MMygind <56871472+MMygind@users.noreply.github.com>
2020-04-30 10:10:25 +0200

    S I N G L E T O N

--- src/timemanagement/GUI/controller/MainAdminViewController.java
+++ src/timemanagement/GUI/controller/MainAdminViewController.java
@@ -149,7 +149,7 @@ public class MainAdminViewController implements Initializable
         {
             timeLoggerPane.toFront();
 
-            model = new Model();
+            model = model.getInstance();
             for (Project projects : model.getAllProjects())
             {
                 projektComboBox.getItems().add(projects.getProjektNavn());
@@ -158,9 +158,6 @@ public class MainAdminViewController implements Initializable
             projekterTableView.setItems(model.getProjectKundeNavn());
             fillColumns();
 
-        } catch (IOException ex)
-        {
-            Logger.getLogger(MainAdminViewController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ModelException ex)
         {
             Logger.getLogger(MainAdminViewController.class.getName()).log(Level.SEVERE, null, ex);

f8756d030e9cec21637d4595d4122cc154c2670c
FimFi <55874743+FimFi@users.noreply.github.com>
2020-05-06 10:25:43 +0200

    Update MainAdminViewController.java

--- src/timemanagement/GUI/controller/MainAdminViewController.java
+++ src/timemanagement/GUI/controller/MainAdminViewController.java
@@ -626,7 +626,6 @@ public class MainAdminViewController implements Initializable
     @FXML
     private void handleCreateProjekt(ActionEvent event) throws ModelException
     {
-        model.createKunde(txt_kundeNavn.getText());
         if(model.createKunde(txt_kundeNavn.getText()) == true)
         {
             model.createProjekt(txt_projektNavn.getText(), model.getKundeId(txt_kundeNavn.getText()), LocalDate.now().toString(), 0, 1, 0);
